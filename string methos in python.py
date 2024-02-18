bec2e9b9ed1be16cbc24704407bea936c5f03fa1
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-18 18:28:48 +0530

    Add files via upload

---
+++ string methos in python.py
@@ -0,0 +1,19 @@
+# Strings are immutable
+a = "Vijay!!!!!!!!!!!"
+print(len(a))
+print(a.upper())    
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Vijay", "Sadashiv"))
+print(a.split(" "))
+
+Kiccha = "abhinay chakravarti"
+print(Kiccha.capitalize())
+
+str1 = "welcome to the console @@@"
+print(str1.endswith("@@@"))
+
+str1 = "welcome to the console@@@"
+print(str1.endswith("to", 4, 10))
+
+ 
